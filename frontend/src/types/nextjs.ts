fde5b8c7c36a5b3ea85443c34f9f6983097085de
곽태욱 <gwak2837@gmail.com>
2024-08-16 02:14:52 +0900

    Update nextjs.ts

--- frontend/src/types/nextjs.ts
+++ frontend/src/types/nextjs.ts
@@ -2,13 +2,10 @@ import type { Locale } from '@/middleware'
 
 import { type ReactNode } from 'react'
 
-export type LayoutProps<
-  Slot extends Record<string, ReactNode> = Record<string, ReactNode>,
-  Param extends Record<string, unknown> = Record<string, string>,
-> = {
+export type LayoutProps<Param extends Record<string, unknown> = Record<string, string>> = {
   children: ReactNode
   params: { locale: Locale } & Param
-} & Slot
+}
 
 export type PageProps<T extends Record<string, unknown> = Record<string, string>> = {
   params: { locale: Locale } & T
