eecb2fb0b37e18daf878909c236cb5296b8160db
곽태욱 <logan.gwak@ridi.com>
2024-08-21 12:38:40 +0900

    google logo svg

--- frontend/src/app/[locale]/(navigation)/login/page.tsx
+++ frontend/src/app/[locale]/(navigation)/login/page.tsx
@@ -24,23 +24,24 @@ export default async function Page({ params }: PageProps) {
             {dict.비바톤_익명인증[locale]}
           </a>
           <button className="flex w-full items-center justify-center gap-2 rounded border border-gray-300 bg-white px-4 py-2 font-medium text-gray-700 transition-transform hover:bg-gray-100 active:scale-95 dark:border-transparent dark:bg-white dark:text-gray-700 dark:hover:bg-gray-200">
-            <svg className="h-5 w-5" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+            <svg className="h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path
-                d="M47.5 24.5c0-1.5-.1-2.9-.3-4.3H24v8.1h13.2c-.5 2.9-2.1 5.4-4.4 7.1v5.9h7.2c4.2-3.9 6.5-9.7 6.5-16.8z"
+                d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
                 fill="#4285F4"
               />
               <path
-                d="M24 48c6 0 11-2 14.6-5.3l-7.2-5.9c-2 1.3-4.7 2.1-7.4 2.1-5.7 0-10.5-3.9-12.2-9.1H4v5.7C7.7 42.1 15.1 48 24 48z"
+                d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
                 fill="#34A853"
               />
               <path
-                d="M11.8 29.8c-1-2.9-1-6.1 0-9.1V15h-7.5C1.1 18.8 0 21.8 0 24.5s1.1 5.7 3.3 9.1l7.5-3.8z"
+                d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
                 fill="#FBBC05"
               />
               <path
-                d="M24 9.5c3 0 5.7 1 7.8 3l5.9-5.9C33.8 2.5 29.8 0 24 0 15.1 0 7.7 5.9 4 14.3l7.5 5.7C13.5 13.4 18.3 9.5 24 9.5z"
+                d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
                 fill="#EA4335"
               />
+              <path d="M1 1h22v22H1z" fill="none" />
             </svg>
             {dict.구글_로그인[locale]}
           </button>
