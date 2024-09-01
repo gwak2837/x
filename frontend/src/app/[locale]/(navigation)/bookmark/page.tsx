b353856803243eb75399e0054ee8738029e901af
곽태욱 <gwak2837@gmail.com>
2024-09-01 14:12:19 +0900

    link

--- frontend/src/app/[locale]/(navigation)/bookmark/page.tsx
+++ frontend/src/app/[locale]/(navigation)/bookmark/page.tsx
@@ -8,7 +8,6 @@ export default async function Page({ params, searchParams }: PageProps) {
 
   return (
     <div className="p-4 sm:p-8 md:p-16 lg:p-24">
-      <Link href={`/${locale}/manga/3029130?max=48`}>3029130</Link>
       <pre className="overflow-x-scroll">{JSON.stringify({ params, searchParams }, null, 2)}</pre>
     </div>
   )
