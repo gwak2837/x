4ffe45eb26c4d987cc9f8ae0b4aad4734b967762
곽태욱 <gwak2837@gmail.com>
2024-09-01 12:40:37 +0900

    d

--- frontend/src/app/[locale]/manga/[id]/page.tsx
+++ frontend/src/app/[locale]/manga/[id]/page.tsx
@@ -5,7 +5,10 @@ import Link from 'next/link'
 
 export default async function Page({ params, searchParams }: PageProps) {
   const { id } = params
-  const i = (searchParams.i ?? '1') as string
+  const i = +(searchParams.i ?? 1)
+  const maxString = searchParams.max ?? '1'
+  const max = +maxString
+  const maxLength = maxString.length
 
   return (
     <div className="relative mx-auto max-w-screen-2xl">
@@ -15,12 +18,20 @@ export default async function Page({ params, searchParams }: PageProps) {
           className={`h-lvh object-contain ${j === 0 ? '' : 'hidden'}`}
           height={1536}
           priority
-          src={`/api/${id}/${+i + j}`}
+          src={`/api/${id}/${String(i + j).padStart(maxLength, '0')}`}
           width={1536}
         />
       ))}
-      <Link className="absolute left-0 top-0 h-full w-1/3" href={`?i=${+i - 1}`} />
-      <Link className="absolute right-0 top-0 h-full w-1/3" href={`?i=${+i + 1}`} />
+      <Link
+        className="absolute left-0 top-0 h-full w-1/3"
+        href={`?max=${maxString}&i=${Math.max(1, i - 1)}`}
+        replace
+      />
+      <Link
+        className="absolute right-0 top-0 h-full w-1/3"
+        href={`?max=${maxString}&i=${Math.min(i + 1, max)}`}
+        replace
+      />
     </div>
   )
 }
