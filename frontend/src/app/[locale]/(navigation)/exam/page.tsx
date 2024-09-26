2380b1141e055a51a0207c61223043c09839f2c2
곽태욱 <gwak2837@gmail.com>
2024-09-26 14:26:21 +0900

    Update page.tsx

--- frontend/src/app/[locale]/(navigation)/exam/page.tsx
+++ frontend/src/app/[locale]/(navigation)/exam/page.tsx
@@ -13,9 +13,7 @@ export default async function Page({ params, searchParams }: PageProps) {
       <main className="h-full">
         <TopNavigation className="border-b border-gray-300 dark:border-gray-700" locale={locale} />
         <div className="h-12" />
-        <Link href={`/${locale}/manga/3029130?max=48`} target="__blank">
-          3029130
-        </Link>
+
         <pre className="overflow-x-scroll">{JSON.stringify({ params, searchParams }, null, 2)}</pre>
       </main>
     </>
