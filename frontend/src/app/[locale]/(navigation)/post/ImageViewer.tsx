ffcd3c71a297ed5ea7d46bb732d83241c8a47ed4
곽태욱 <gwak2837@gmail.com>
2024-10-05 00:48:21 +0900

    keyboard event

--- frontend/src/app/[locale]/(navigation)/post/ImageViewer.tsx
+++ frontend/src/app/[locale]/(navigation)/post/ImageViewer.tsx
@@ -2,13 +2,13 @@
 
 import type { TPost } from '@/mock/post'
 
+import KeybordShortcut from '@/components/KeybordShortcut'
 import useFetchWithAuth from '@/hook/useFetchWithAuth'
-import IconAngleBracket from '@/svg/IconAngleBracket'
 import IconArrow from '@/svg/IconArrow'
 import IconDoubleAngleBrackets from '@/svg/IconDoubleAngleBrackets'
 import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 type Props = {
   urls: string[]
@@ -16,10 +16,11 @@ type Props = {
 }
 
 export default function ImageViewer({ urls, initialPost }: Props) {
-  const fetchWithAuth = useFetchWithAuth()
   const [isVisible, setIsVisible] = useState(false)
-  const [imageIndex, setImageIndex] = useState(0)
-  console.log('👀 - imageIndex:', imageIndex)
+  const imageContainerRef = useRef<HTMLDivElement>(null)
+  const imageContainer = imageContainerRef.current
+
+  const fetchWithAuth = useFetchWithAuth()
 
   const { data: post } = useQuery({
     queryKey: ['post', initialPost.id],
@@ -28,32 +29,52 @@ export default function ImageViewer({ urls, initialPost }: Props) {
     enabled: isVisible,
   })
 
-  const className = 'absolute hidden rounded-full bg-white/50 lg:block dark:bg-black/50'
+  const className = 'absolute  rounded-full bg-white/50  dark:bg-black/50'
+
+  function scrollToLeft() {
+    imageContainer?.scrollTo({
+      left: imageContainer.scrollLeft - imageContainer.clientWidth,
+      behavior: 'smooth',
+    })
+  }
+
+  function scrollToRight() {
+    imageContainer?.scrollTo({
+      left: imageContainer.scrollLeft + imageContainer.clientWidth,
+      behavior: 'smooth',
+    })
+  }
 
   return (
     <div className="fixed inset-0 z-10 grid grid-cols-[1fr_auto]">
       <div className="relative">
-        <div className="flex h-full snap-x snap-mandatory overflow-x-auto">
+        <div className="flex h-full snap-x snap-mandatory overflow-x-auto" ref={imageContainerRef}>
+          <div />
           {urls.map((url, i) => (
             <div className="relative w-full flex-shrink-0 snap-center snap-always" key={i}>
               <Image alt="post-image" className="object-contain" fill src={url} />
             </div>
           ))}
+          <div />
         </div>
+        <KeybordShortcut keyCode="ArrowLeft" onKeyDown={scrollToLeft}>
+          <button
+            className={`${className} left-4 top-1/2 -translate-y-1/2 p-2`}
+            onClick={scrollToLeft}
+          >
+            <IconArrow className="w-5" />
+          </button>
+        </KeybordShortcut>
+        <KeybordShortcut keyCode="ArrowRight" onKeyDown={scrollToRight}>
+          <button
+            className={`${className} right-4 top-1/2 -translate-y-1/2 p-2`}
+            onClick={scrollToRight}
+          >
+            <IconArrow className="w-5 rotate-180" />
+          </button>
+        </KeybordShortcut>
         <button
-          className={`${className} left-4 top-1/2 -translate-y-1/2 p-2`}
-          onClick={() => setImageIndex((prev) => (prev + urls.length - 1) % urls.length)}
-        >
-          <IconArrow className="w-5" />
-        </button>
-        <button
-          className={`${className} right-4 top-1/2 -translate-y-1/2 p-2`}
-          onClick={() => setImageIndex((prev) => (prev + 1) % urls.length)}
-        >
-          <IconArrow className="w-5 rotate-180" />
-        </button>
-        <button
-          className={`${className} right-3 top-3 p-1`}
+          className={`${className} right-3 top-3 hidden p-1 lg:block`}
           onClick={() => setIsVisible((prev) => !prev)}
         >
           <IconDoubleAngleBrackets
--- frontend/src/components/KeybordShortcut.tsx
+++ frontend/src/components/KeybordShortcut.tsx
@@ -4,19 +4,21 @@ import { type ReactNode, useEffect } from 'react'
 
 type Props = {
   children: ReactNode
+  withAltKey?: boolean
   keyCode: string
   onKeyDown?: () => void
 }
 
-export default function KeybordShortcut({ children, keyCode, onKeyDown }: Props) {
+export default function KeybordShortcut({ withAltKey, children, keyCode, onKeyDown }: Props) {
   useEffect(() => {
     function downHandler({ code, altKey }: KeyboardEvent) {
-      if (altKey && keyCode === code) {
+      if ((!withAltKey || altKey) && keyCode === code) {
         onKeyDown?.()
       }
     }
 
     document.addEventListener('keydown', downHandler)
+
     return () => {
       document.removeEventListener('keydown', downHandler)
     }
