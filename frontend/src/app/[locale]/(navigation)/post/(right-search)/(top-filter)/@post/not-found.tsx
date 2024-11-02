df52728f5ed47420ed8cf1bd55470151eef440b6
곽태욱 <gwak2837@gmail.com>
2024-11-02 09:31:34 +0900

    Update not-found.tsx

--- frontend/src/app/[locale]/(navigation)/post/(right-search)/(top-filter)/@post/not-found.tsx
+++ frontend/src/app/[locale]/(navigation)/post/(right-search)/(top-filter)/@post/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="col-span-full m-10 flex min-h-[50vh] flex-col items-center justify-center text-gray-900 lg:min-w-[65ch] dark:text-white">
+    <div className="col-span-full m-10 flex min-h-[50vh] flex-col items-center justify-center text-gray-900 dark:text-white">
       <h1 className="mb-4 text-6xl font-bold">404</h1>
       <p className="mb-8 text-2xl">게시글이 없어요</p>
       <div className="grid gap-2">
